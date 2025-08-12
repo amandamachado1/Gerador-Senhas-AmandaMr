cdb1922d346e28944dc335a92085a32abcd9dbe1
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-08-12 08:53:24 -0300

    aaaaa

---
+++ main.js
@@ -0,0 +1,21 @@
+const numeroSenha = document.querySelectorAll('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.texContent = tamanhoSenha;
+
+const botoes = documen.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuitamanho;
+botoes[1].onclick = aumentatamnho;
+
+function diminuitamanho(){
+if(tamanhoSenha > 1){
+tamanhoSenha = tamanhoSenha - 1;
+
+numeroSenha.texContent-tamanhoSenha;
+
+
+}
+
+
+}
+
