aebe6cd6e5ae423d0ae77363a73a2f8b9b881ed0
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-08-26 07:57:28 -0300

    26/08

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.texContent = tamanhoSenha;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamnho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1;
     }
-    numeroSenha.texContent = tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
 }
-
-
-
